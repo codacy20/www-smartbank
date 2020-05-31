be086342f5d40961b1ee0b246c64a131881d1e3b
codacy20 <kmuc72@gmail.com>
2020-06-01 01:13:41 +0200

    navigation and active state

--- src/app/common/navigation/navigation.component.ts
+++ src/app/common/navigation/navigation.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { UrlTree, Router } from '@angular/router';
 
 @Component({
   selector: 'app-navigation',
@@ -6,10 +7,40 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navigation.component.scss']
 })
 export class NavigationComponent implements OnInit {
+  public active = true;
+  current: string;
 
-  constructor() { }
+  constructor(private router: Router) {
+    this.current = location.href.substring(location.href.lastIndexOf('/') + 1);
+  }
 
   ngOnInit() {
+  }
+
+  navigate(input: number) {
+    switch (input) {
+      case 0:
+        this.router.navigateByUrl('/dashboard');
+        break;
+      case 1:
+        this.router.navigateByUrl('/payments');
+        break;
+      case 2:
+        this.router.navigateByUrl('/offers');
+        break;
+      case 3:
+        this.router.navigateByUrl('/chat');
+        break;
+      case 4:
+        this.router.navigateByUrl('/map');
+        break;
+      case 5:
+        this.router.navigateByUrl('/setting');
+        break;
+      case 6:
+        this.router.navigateByUrl('/login');
+        break;
+    }
   }
 
 }
